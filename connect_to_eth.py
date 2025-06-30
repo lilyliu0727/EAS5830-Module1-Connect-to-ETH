1aaab6db8d585c375d92b4cdc41a73c5124f1607
lilyliu0727 <lilyliu0727@gmail.com>
2025-06-29 22:03:54 -0400

    Add files via upload

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -9,10 +9,9 @@ now_url  = f"https://eth.nownodes.io/{now_token}"
 alchemy_url = f"https://eth-mainnet.alchemyapi.io/v2/{alchemy_token}"
 infura_url = f"https://mainnet.infura.io/v3/{infura_token}"
 '''
-
+alchemy_token = "wPmwdVjzw2L4K63VSfrjSWiVBLGcnpDU"
 
 def connect_to_eth():
-	alchemy_token = "wPmwdVjzw2L4K63VSfrjSWiVBLGcnpDU"
 	url = f"https://eth-mainnet.alchemyapi.io/v2/{alchemy_token}"
 	
 	w3 = Web3(HTTPProvider(url))
@@ -29,17 +28,20 @@ def connect_with_middleware(contract_json):
 
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	w3 = 0
+	bnb_test_url  = f"https://bnb-testnet.g.alchemy.com/v2/{alchemy_token}"
+	w3 = Web3(HTTPProvider(bnb_test_url))
+	assert w3.is_connected(), f"Failed to connect to provider at {bnb_test_url}"
 
 	# The second section requires you to inject middleware into your w3 object and
 	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
 	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
-	contract = 0
+
+	w3.middleware_onion.inject(web3.middleware.ExtraDataToPOAMiddleware, layer=0)
+	contract = w3.eth.contract(address=address, abi=abi)
 
 	return w3, contract
 
 
 if __name__ == "__main__":
-
 
 	connect_to_eth()
